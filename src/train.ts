7203553a59e9703ee196ea69328d7258ac9dea30
ziynatilloh-html <ziynatillokht@gmail.com>
2024-12-08 16:54:20 +0900

    feat: Task-H done

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,15 @@
-console.log("qalesan");
+//Task-H
+
+function getPositive(arr: number[]): number[] {
+  let result: number[] = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+}
+
+console.log(getPositive([1, -4, 2]));
+
+//!https://stackoverflow.com/questions/71670965/how-do-i-assert-a-number-type-is-positive
