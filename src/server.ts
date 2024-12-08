ea1fb7110da792795bed186190b8359ab5202dc9
ziynatilloh-html <ziynatillokht@gmail.com>
2024-12-08 16:11:20 +0900

    feat: connect mongoDB via mongoose

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-console.log("PORT", process.env.PORT);
-console.log("MONGO_URL", process.env.MONGO_URL);
+import mongoose from "mongoose";
+mongoose
+  .connect(process.env.MONGO_URL as string, {})
+  .then((data) => {
+    console.log("MongoDB connection succeed");
+    const PORT = process.env.PORT ?? 3003;
+  })
+  .catch((err) => console.log("ERROR on connection MongoDB", err));
