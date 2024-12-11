a6d28927b090ebe4aafa8d001999efe627aa82b4
ziynatilloh-html <ziynatillokht@gmail.com>
2024-12-11 17:02:35 +0900

    feat: TASK_H2 is completed scd sdc

--- src/train.ts
+++ src/train.ts
@@ -1,15 +1,24 @@
-//Task-H
+//TASK-h2
 
-function getPositive(arr: number[]): number[] {
-  let result: number[] = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > 0) {
-      result.push(arr[i]);
-    }
-  }
-  return result;
+function getDigits(input: string): string {
+  let matches = input.match(/\d+/g);
+  return matches ? matches.join("") : "";
 }
 
-console.log(getPositive([1, -4, 2]));
+console.log(getDigits("mahkam123456topshiriqlar789kelgusi2024"));
 
-//!https://stackoverflow.com/questions/71670965/how-do-i-assert-a-number-type-is-positive
+//Task-H
+
+// function getPositive(arr: number[]): number[] {
+//   let result: number[] = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > 0) {
+//       result.push(arr[i]);
+//     }
+//   }
+//   return result;
+// }
+
+// console.log(getPositive([1, -4, 2]));
+
+// //!https://stackoverflow.com/questions/71670965/how-do-i-assert-a-number-type-is-positive
