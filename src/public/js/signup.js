9b49635e3e50baf07de8d26105410eebf9fce1c4
ziynatilloh-html <ziynatillokht@gmail.com>
2025-01-03 15:45:06 +0900

    fix: frontend signup image validation logic

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,12 +1,35 @@
 console.log("Signup frontend javascript file");
-$(function () {});
+$(function () {
+  const fileTarget = $(".file-box .upload-hidden");
+  let filename;
+
+  fileTarget.on("change", function () {
+    if (window.FileReader) {
+      const uploadFile = $(this)[0].files[0];
+      const fileType = uploadFile["type"];
+      const validImageType = ["image/jpg", "image/jpeg", "image/png"];
+      if (!validImageType.includes(fileType)) {
+        alert("Please insert only jpeg, jpg and png!");
+      } else {
+        if (uploadFile) {
+          console.log(URL.createObjectURL(uploadFile));
+          $(".upload-img-frame")
+            .attr("src", URL.createObjectURL(uploadFile))
+            .addClass("success");
+        }
+        filename = $(this)[0].files[0].name;
+      }
+    }
+
+    $(this).siblings(".upload-name").val(filename);
+  });
+});
 
 function validateSignupForm() {
   const memberNick = $(".member-nick").val();
   const memberPhone = $(".member-phone").val();
   const memberPassword = $(".member-password").val();
   const confirmPassword = $(".confirm-password").val();
-
   if (
     memberNick === "" ||
     memberPhone === "" ||
@@ -19,6 +42,14 @@ function validateSignupForm() {
 
   if (memberPassword !== confirmPassword) {
     alert("Password differs, please check!");
+    return false;
+  }
+  const memberImage = $(".member-image").get(0).files[0]?.name
+    ? $(".member-image").get(0).files[0].name
+    : null;
+
+  if (!memberImage) {
+    alert("Please insert restaurant image!");
     return false;
   }
 }
