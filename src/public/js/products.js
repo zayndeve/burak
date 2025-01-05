17b61882577f7b491db0741e8817be6f810b84ee
ziynatilloh-html <ziynatillokht@gmail.com>
2025-01-05 17:38:38 +0900

    feat: update chosen product frontend logic

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -20,6 +20,29 @@ $("#cancel-btn").on("click", () => {
   $(".dish-container").slideToggle(100);
   $("#process-btn").css("display", "flex");
 });
+$(".new-product-status").on("change", async function (e) {
+  const id = e.target.id;
+  const productStatus = $(`#${id}.new-product-status`).val();
+  console.log("id:", id);
+  console.log("productStatus:", productStatus);
+
+  try {
+    const response = await axios.post(`/admin/product/${id}`, {
+      productStatus: productStatus,
+    });
+    console.log("response:", response);
+    const result = response.data;
+    if (result.data) {
+      console.log("Product updated!");
+      $(".new-product-status").blur();
+    } else {
+      alert("Product update failed!");
+    }
+  } catch (err) {
+    console.log(err);
+    alert("Product update failed!");
+  }
+});
 
 function validateForm() {
   const productName = $(".product-name").val();
