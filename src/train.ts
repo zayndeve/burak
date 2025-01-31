3c82b52ed69eddd8adab3ff9b8603a0a6b12e869
ziynatilloh-html <ziynatillokht@gmail.com>
2025-01-31 18:11:03 +0900

    feat: Task_t completed

--- src/train.ts
+++ src/train.ts
@@ -1,9 +1,17 @@
-/* Task_Y */
-function findIntersection(arr1: number[], arr2: number[]): number[] {
-  return arr1.filter((value) => arr2.includes(value));
+/!Task_T/;
+function sumEvens(numbers: number[]): number {
+  return numbers
+    .filter((num) => num % 2 === 0)
+    .reduce((sum, num) => sum + num, 0);
 }
-const result = findIntersection([1, 2, 3], [3, 2, 0]);
-console.log(result);
+console.log(sumEvens([4, 4]));
+console.log(sumEvens([1, 2, 3, 2]));
+/* Task_Y */
+// function findIntersection(arr1: number[], arr2: number[]): number[] {
+//   return arr1.filter((value) => arr2.includes(value));
+// }
+// const result = findIntersection([1, 2, 3], [3, 2, 0]);
+// console.log(result);
 // function countOccurrences(obj: any, key: string): number {
 //   let count = 0;
 
